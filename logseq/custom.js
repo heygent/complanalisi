42ac219316db441efad7d9b670301af1bab0aac8
heygent <emanuele.gentiletti@gmail.com>
2023-09-08 09:59:39 +0200

    Auto saved by Logseq

--- logseq/custom.js
+++ logseq/custom.js
@@ -8,10 +8,39 @@ function whenAvailable(name, callback) {
   }, 100);
 }
 
+// Rewrite the following for Katex:
+//
+// /\newcommand{\R}{\mathbb{R}}
+// /\newcommand{\C}{\mathbb{C}}
+// /\newcommand{\Z}{\mathbb{Z}}
+// /\newcommand{\N}{\mathbb{N}}
+// /\newcommand{\L}[2][I]{ {L^{#2}(#1)} }
+// /\newcommand{\Li}[1][I]{\L[#1]{\infty}}
+// /\newcommand{\norm}[1][\ldots]{\lVert#1\rVert}
+// /\newcommand{\lnorm}[1][\ldots]{\left\lVert#1\right\rVert}
+// /\newcommand{\expfn}[1]{\exp\left(#1\right)}
+// /\newcommand{\F}{\mathcal{F}}
+// /\newcommand{\fint}{\int_{-\infty}^{+\infty}}
+// /\newcommand{\l}{\left}
+// /\newcommand{\r}{\right}
+// /\DeclareMathOperator*{\esssup}{ess\,sup}
+
 const macros = {
   "\\R": "\\mathbb{R}",
   "\\C": "\\mathbb{C}",
   "\\t": "\\text{#1}",
+  "\\Z": "\\mathbb{Z}",
+  "\\N": "\\mathbb{N}",
+  "\\L": "\\L^{#2}(#1)",
+  "\\Li": "\\L[#1]{\\infty}",
+  "\\norm": "\\lVert#1\\rVert",
+  "\\lnorm": "\\left\\lVert#1\\right\\rVert",
+  "\\expfn": "\\exp\\left(#1\\right)",
+  "\\F": "\\mathcal{F}",
+  "\\fint": "\\int_{-\\infty}^{+\\infty}",
+  "\\l": "\\left",
+  "\\r": "\\right",
+  "\\esssup": "\\ess\\,sup",
 }
 
 
