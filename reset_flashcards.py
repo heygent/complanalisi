c9386b4199a96eec818be41b427d3470b0d506d4
heygent <emanuele.gentiletti@gmail.com>
2023-09-13 09:15:09 +0200

    Revert "Revert "Auto saved by Logseq""
    This reverts commit 2839d38cfe4a9978d92ebece6625fc021b535f4f.

--- reset_flashcards.py
+++ reset_flashcards.py
@@ -1,8 +1,25 @@
+import re
 from pathlib import Path
 
+folder_path = Path("/path/to/your/folder")
 
-PAGES_DIR = Path(__file__).parent / "pages"
+pattern = re.compile(r"card-next-schedule:: (\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z)")
 
+# Replacement string with the new date
+replacement = "card-next-schedule:: 2023-09-01T"
 
-for page in PAGES_DIR.glob("*.md"):
-    with page.open() as f:
+# Iterate through all files in the folder
+for file_path in folder_path.iterdir():
+    # Check if the item is a file (not a subfolder)
+    if file_path.is_file():
+        with file_path.open('r') as file:
+            lines = file.readlines()
+        
+        with file_path.open('w') as file:
+            for line in lines:
+                # Use compiled regex to find and replace the date portion
+                modified_line = pattern.sub(replacement, line)
+                print(modified_line)
+                # file.write(modified_line)
+
+print("Date replacement completed.")
