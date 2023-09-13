c85817f240bf7b08ad2aecf0b407a6c70be31f47
heygent <emanuele.gentiletti@gmail.com>
2023-09-13 09:21:00 +0200

    Revert "Auto saved by Logseq"
    This reverts commit 82de3ec2893cd33d03a06889a1635c85069ca4f0.

---
+++ pages/Principio di Indeterminazione.md
@@ -0,0 +1,4 @@
+- ![image.png](../assets/image_1694459881387_0.png)
+- ![image.png](../assets/image_1694470150801_0.png)
+- ![image.png](../assets/image_1694470654559_0.png)
+-
--- reset_flashcards.py
+++ reset_flashcards.py
@@ -1,25 +1,8 @@
-import re
 from pathlib import Path
 
-folder_path = Path(__file__).parent / "pages"
 
-pattern = re.compile(r"card-next-schedule:: (\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z)")
+PAGES_DIR = Path(__file__).parent / "pages"
 
-# Replacement string with the new date
-replacement = "card-next-schedule:: 2023-09-01T"
 
-# Iterate through all files in the folder
-for file_path in folder_path.glob('**/*.md'):
-    # Check if the item is a file (not a subfolder)
-    if file_path.is_file():
-        with file_path.open('r') as file:
-            lines = file.readlines()
-        
-        with file_path.open('w') as file:
-            for line in lines:
-                # Use compiled regex to find and replace the date portion
-                modified_line = pattern.sub(replacement, line)
-                print(modified_line)
-                # file.write(modified_line)
-
-print("Date replacement completed.")
+for page in PAGES_DIR.glob("*.md"):
+    with page.open() as f:
